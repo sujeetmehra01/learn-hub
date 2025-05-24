a3a255ea5f7c747ac8130259ee7a1508fd3d4b64
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2025-05-24 21:25:50 +0530

    NotFound page created and all pages maped with the route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Pricing from "./pages/pricing/Pricing";
+import Courses from "./pages/courses/Courses";
+import Login from "./pages/auth/login/Login";
+import Signup from "./pages/auth/signup/Signup";
+import NotFound from "./pages/notfound/NotFound";
 
 function App() {
   return (
     <>
-    <h1>Welcome to learnHub</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login/>} />
+          <Route path="signup" element={<Signup/>} />
+          <Route path="courses" element={<Courses/>} />
+          <Route path="pricing" element={<Pricing/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
